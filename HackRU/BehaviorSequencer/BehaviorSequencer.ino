78f3b12906b14f14e45d322e0ea2926c5a16462c
Michael Gubbels <mrgubbels@gmail.com>
2013-10-13 00:11:33 -0400

    HackRU, Day 2 (Sunday). Integrated accelerometer code into main node sketch.

--- HackRU/BehaviorSequencer/BehaviorSequencer.ino
+++ HackRU/BehaviorSequencer/BehaviorSequencer.ino
@@ -1,6 +1,16 @@
 #include <MovingAvarageFilter.h>
 
+#define ACCELEROMETER_X_PIN A3
+#define ACCELEROMETER_Y_PIN A2
+#define ACCELEROMETER_Z_PIN A1
 #define RELAY_ENABLE_PIN 12
+
+// these constants describe the pins. They won't change:
+//const int groundpin = 18;             // analog input pin 4 -- ground
+//const int powerpin = 19;              // analog input pin 5 -- voltage
+//const int xpin = A3;                  // x-axis of the accelerometer
+//const int ypin = A2;                  // y-axis
+//const int zpin = A1;                  // z-axis (only on 3-axis models)
 
 MovingAvarageFilter movingAvarageFilter(20);
 
@@ -39,10 +49,17 @@ void loop() {
   }
   
   
+  //
+  // Print accelerometer data over serial
+  //
   
-  
-//  digitalWrite(RELAY_ENABLE_PIN, LOW);
-//  delay(500);
-//  digitalWrite(RELAY_ENABLE_PIN, HIGH);
-//  delay(500);
+  // print the sensor values:
+  Serial.print(analogRead(ACCELEROMETER_X_PIN));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(ACCELEROMETER_Y_PIN));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(ACCELEROMETER_Z_PIN));
+  Serial.println();
 }
